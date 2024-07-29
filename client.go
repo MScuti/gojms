a5d91b3c5dca2c5323a151b4c1fed4046ebafb85
MScuit <badskyer@fireunix.net>
2024-07-29 10:47:46 +0800

    Add JmsSdkClient struct and NewSdkClient factory function
    Introduced the JmsSdkClient struct for enhanced SDK client management, paralleling the existing JmsAKClient structure. Accompanied this with a NewSdkClient factory function to initialize JmsSdkClient instances, improving modularity and clarity in client setup.

--- client.go
+++ client.go
@@ -41,7 +41,54 @@ type JmsClient struct {
 	User     User
 }
 
+// JmsAKClient is a struct representing a AKClient entity in the program.
+// It includes properties that a AKClient entity may have.
+//
+// Properties:
+//
+//	Terminal: This property comprises a Terminal structure for terminal session operations.
+//	Account: This property holds the Account struct for account operations.
+//	Assets: This property contains the Assets structure for asset management operations.
+//	User: This property uses the User struct for user management operations.
+//
+// The struct has been developed to enable easy management and interaction with terminals, accounts,
+// assets, and users.
+//
+// Each field in the struct is equipped to manage and interact with their respective properties.
+// The struct is initialized using a factory function NewJmsAKClient which takes in a parameter of
+// apiauth.JmsAKConfig type.
+//
+// The significance of each field is defined in their respective structs 'Terminal','Account','Assets'
+// and 'User'. Each struct has corresponding methods for different operations like fetching data from
+// the server, making HTTP requests, etc.
 type JmsAKClient struct {
+	Terminal Terminal
+	Account  Account
+	Assets   Assets
+	User     User
+}
+
+// JmsSdkClient is a struct representing a SdkClient entity in the program.
+// It includes properties that a SdkClient entity may have.
+//
+// Properties:
+//
+//	Terminal: This property comprises a Terminal structure for terminal session operations.
+//	Account: This property holds the Account struct for account operations.
+//	Assets: This property contains the Assets structure for asset management operations.
+//	User: This property uses the User struct for user management operations.
+//
+// The struct has been developed to enable ease in managing and interacting with terminals, accounts,
+// assets, and users.
+//
+// Each field in the struct is equipped to manage and interact with their respective properties.
+// The struct is initialized using a factory function NewJmsSdkClient which takes in a parameter of
+// apiauth.JmsSdkConfig type.
+//
+// The significance of each field is defined in their respective structs 'Terminal','Account','Assets'
+// and 'User'. Each struct has corresponding methods for different operations like fetching data from
+// the server, making HTTP requests, etc.
+type JmsSdkClient struct {
 	Terminal Terminal
 	Account  Account
 	Assets   Assets
@@ -77,6 +124,44 @@ func NewJmsClient(api apiauth.JmsAPIConfig) *JmsClient {
 // NewJmsAKClient is a factory function that returns a new NewJmsAKClient.
 func NewJmsAKClient(api apiauth.JmsAKConfig) *JmsAKClient {
 	return &JmsAKClient{
+		Terminal: Terminal{
+			Session: terminal.Sessions{
+				API: &api,
+			},
+		},
+		Account: Account{
+			Account: accouts.Account{
+				API: &api,
+			},
+		},
+		Assets: Assets{
+			Assets: assets.Assets{
+				API: &api,
+			},
+		},
+		User: User{
+			User: users.User{API: &api},
+		},
+	}
+}
+
+// NewSdkClient is a function that initializes a new JmsSdkClient struct
+// with the provided JmsSDKConfig and returns a pointer to it.
+//
+// Parameters:
+//
+//	api apiauth.JmsSDKConfig: An instance of JmsSDKConfig containing API configuration parameters.
+//
+// Returns:
+//
+//	*JmsSdkClient: A pointer to the newly initialized JmsSdkClient struct.
+//
+// Process:
+//   - The function initializes a new JmsSdkClient struct with the provided api, setting up the Terminal, Account, Assets, and User fields.
+//   - The provided JmsSDKConfig is set as the API for the Sessions field of Terminal, Account field of Account, Assets field of Assets, and User field of User.
+//   - Finally, the function returns a pointer to this newly initialized JmsSdkClient struct.
+func NewSdkClient(api apiauth.JmsSDKConfig) *JmsSdkClient {
+	return &JmsSdkClient{
 		Terminal: Terminal{
 			Session: terminal.Sessions{
 				API: &api,
